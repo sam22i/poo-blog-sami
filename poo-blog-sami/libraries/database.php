db5b040e37449dd6b5f993a12082c02e5b6bcb06
sam22i <114009284+sam22i@users.noreply.github.com>
2024-02-18 19:21:35 +0100

    Add files via upload

---
+++ poo-blog-sami/libraries/database.php
@@ -0,0 +1,89 @@
+<?php
+
+/**
+ * retourne une connexion à la base de donnée
+ *
+ * @return PDO
+ */
+function getPdo():PDO {
+
+    $pdo = new PDO('mysql:host=localhost;dbname=blogpoo;charset=utf8', 'root', '', [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC]);
+
+        return $pdo;
+}
+/**
+ * retourne la liste des articles classés par date de parution
+ *
+ * @return array
+ */
+function findAllArticles(): array{
+    $pdo = getPdo();
+    $resultats = $pdo->query('SELECT * FROM articles ORDER BY created_at DESC');
+    // On fouille le résultat pour en extraire les données réelles
+    $articles = $resultats->fetchAll();
+
+    return $articles;
+}
+
+function findArticle(int $id, ) : array{
+
+    $pdo = getPdo();
+
+    $query = $pdo->prepare("SELECT * FROM articles WHERE id = :article_id");
+
+    // On exécute la requête en précisant le paramètre :article_id 
+    $query->execute(['article_id' => $id]);
+
+    // On fouille le résultat pour en extraire les données réelles de l'article
+    $article = $query->fetch();
+
+    return $article;
+
+}
+ 
+function findAllComments(int $article_id) : array{
+    $pdo = getPdo();
+    $query = $pdo->prepare("SELECT * FROM comments WHERE article_id = :article_id");
+    $query->execute(['article_id' => $article_id]);
+    $commentaires = $query->fetchAll();
+    
+    return $commentaires;
+
+}
+
+function deleteArticle(int $id) {
+    $pdo = getPdo();
+    $query = $pdo->prepare('DELETE FROM articles WHERE id = :id');
+    $query->execute(['id' => $id]);
+
+}
+
+function findComment(int $id) {
+    
+    $pdo = getpdo();
+    $query = $pdo->prepare('SELECT * FROM comments WHERE id = :id');
+    $query->execute(['id' => $id]);
+    $comment = $query->fetch();
+
+    return $comment;
+
+}
+
+function deleteComment(int $id) : void{
+
+    $pdo = getPdo();
+    $query = $pdo->prepare('DELETE FROM comments WHERE id = :id');
+    $query->execute(['id' => $id]);
+
+}
+
+function insertComment (string $autor, string $content, int $article_id) : void {
+
+    $pdo = getPdo();
+    $query = $pdo->prepare('INSERT INTO comments SET author = :author, content = :content, article_id = :article_id, created_at = NOW()');
+    $query->execute(compact('author', 'content', 'article_id'));
+
+
+}
